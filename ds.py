412fc6fc9a2f85379aeedfc7f2e5e8120018781d
D3D_M4N <48412122+user421921@users.noreply.github.com>
2025-09-26 17:05:48 -0700

    Create ds.py
    wip

---
+++ ds.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+#authors user421921, d3dm4n
+# hack the world
+# todo:
+from os import system as c
+import sleep
+import scapy
+def startup():
+  print("establishing uplink")
+  sleep(1)
+  print(".")
+  sleep(1)
+  c("timg -C -t25 v3.mp4")
+def menu():
+  print(""" ---menu---
+  1. Search for Cameras
+  2. Generate Identity
+  3. Jam comms
+  4. Greenlight
+  5. 
+
+        """)
+def main():
+  startup()
+  
+  
