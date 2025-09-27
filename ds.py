a6952be39a9079d230f780185517147bb636bd16
D3D_M4N <48412122+user421921@users.noreply.github.com>
2025-09-27 13:35:09 -0700

    Update ds.py

--- ds.py
+++ ds.py
@@ -3,25 +3,39 @@
 # hack the world
 # todo:
 from os import system as c
-import sleep
-import scapy
+from time import sleep
 def startup():
-  print("establishing uplink")
-  sleep(1)
-  print(".")
-  sleep(1)
+    frames = ["establishing uplink", "establishing uplink.", "establishing uplink..", "establishing uplink..."]
+    for frame in frames:
+        # clear screen
+        #c("cls" if os.name == "nt" else "clear")
+        c("clear")
+        print(frame)
+        sleep(1)
   c("timg -C -t25 v3.mp4")
+
+
 def menu():
   print(""" ---menu---
-  1. Search for Cameras
-  2. Generate Identity
-  3. Jam comms
-  4. Greenlight
-  5. Phone
-  6. 
-
-        """)
+  1. Search for (C)ameras
+  2. Generate (I)dentity
+  3. (J)am comms
+  4. (G)reenlight
+  5. Loop (A)nimation
+  6. (T)arget Identity
+  """)
+  
+def help():
+  print("""
+  1. Search for (C)ameras - scans local network for unencrypted camera frames.
+  2. Generate (I)dentity - generates Fake Idendity for Privacy
+  3. (J)am comms - Jams local WiFi, Bluetooth, and Radio Comms
+  4. (G)reenlight - Triggers Green Light on traffic nearby traffic lights
+  5. Loop (A)nimation - Shows a looped WD themed animation
+  6. (T)arget Identity - Takes information about identity and sends a mass attack.
+  """)
 def main():
   startup()
   
   
+
